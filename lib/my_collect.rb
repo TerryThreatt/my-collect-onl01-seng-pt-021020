af3a6fe73d0f9f6f3c1344c96491dcd3c82c7d88
Terry Threatt <terry.threatt@gmail.com>
2020-02-20 12:01:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(array) 
   i = 0 
-  collection = []
-  
+  new_array = []
   while i < array.length 
-    collection << yield(array[i])
-    i = i + 
+    new_array << yield(array[i])
+    i = i + 1
   end 
-  collection
+  new_array
 end 
 
